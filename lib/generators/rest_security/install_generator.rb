bf0fb32de4c54014762717373c4ad660f85bc59c
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2012-01-30 23:08:59 -0300

    Se agregan los templates para la migración de Oauth::Nonce Se agregan los templates para config/initializers/omniauth.rb Se agregan los templates para config/services.yml Se agregan las dependencias para:
    * oauth/applications
    * oauth/nonce
    * omniauth/strategies/aps

---
+++ lib/generators/rest_security/install_generator.rb
@@ -0,0 +1,42 @@
+require 'rails/generators/active_record'
+
+module RestSecurity
+  class InstallGenerator < Rails::Generators::Base
+
+    include Rails::Generators::Migration
+    
+    source_root File.expand_path('../templates', __FILE__)
+
+    desc 'add the oauth_nonce table'
+
+    def copy_oauth_nonce_migration
+      migration_template 'migration.rb', 'db/migrate/create_oauth_nonces'
+    end
+  
+    def copy_omniauth_initializer
+      template 'omniauth_initializer.rb', 'config/initializers/omniauth.rb'
+    end
+
+    def copy_service_yml_config
+      template 'services.yml', 'config/services.yml'
+    end
+
+    def show_readme
+      readme 'README' if behavior == :invoke
+    end
+
+    #def copy_oauth_nonce_model
+    #  template 'oauth_nonce.rb', 'app/models/oauth/nonce.rb'
+    #end
+
+    def self.next_migration_number(path)
+      unless @prev_migration_nr
+        @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+      else
+        @prev_migration_nr += 1
+      end
+      @prev_migration_nr.to_s
+    end
+    
+  end
+end
--- lib/oauth-rest-security.rb
+++ lib/oauth-rest-security.rb
@@ -1,2 +1,11 @@
+require 'rest_resource'
+require 'oauth/applications'
+require 'oauth/nonce'
+require 'rest-client'
+require 'oauth'
+require 'omniauth'
+require 'omniauth-oauth2'
+require 'omniauth/strategies/aps'
+
 module OauthRestSecurity
 end
